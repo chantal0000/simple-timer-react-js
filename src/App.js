91c764bc1dc5955de5cb41fd7b98f5d6b5551788
Codingwithchantal <c_buerger@gmx.net>
2022-12-21 10:59:20 +0100

    added styled components timer and wrapper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
 import {useEffect, useState} from "react";
+import styled from "styled-components";
+
+const Timer = styled.div`
+  background-color: papayawhip;
+  border: 5px solid deeppink ;
+  border-radius: 50%;
+  margin: 10em;
+  padding: 10em;
+  
+`
+const Wrapper = styled.div`
+  background-color: pink;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  flex-direction: column;
+`
 
 function App() {
+
+
     let future = Date.parse("Dec 24, 2022 11:30:00")
     let now = new Date(); /*calling the now time with the entire format */
     let timeDifference = future - now; /*substraction needed to calculate the days/hours/min/second value that will be implemented in the html */
@@ -29,16 +48,19 @@ function App() {
 
 
     return (<>
+            <Wrapper>
             <div className="App">
-                <div className="timer" id="timer">
-                    <div>{dayTime} Days</div>
-                    <div>{hourTime} Hours</div>
-                    <div>{minuteTime} Minutes</div>
-                    <div>{secondsTime} Seconds</div>
-
-
-                </div>
+                <Timer>
+                    <div className="Timer" id="Timer">
+                        <div>{dayTime} Days</div>
+                        <div>{hourTime} Hours</div>
+                        <div>{minuteTime} Minutes</div>
+                        <div>{secondsTime} Seconds</div>
+                    </div>
+                </Timer>
             </div>
+            </Wrapper>
+
         </>
     );
 }
